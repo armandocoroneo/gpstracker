6efcc4516349b28cfc4618181e8ab2582bb8d177
armandocoroneo <armandocoroneo@gmail.com>
2026-07-30 10:14:22 -0400

    Add files via upload

--- app/src/main/java/com/example/gpstracker/KmlExporter.kt
+++ app/src/main/java/com/example/gpstracker/KmlExporter.kt
@@ -2,34 +2,21 @@ package com.example.gpstracker
 
 object KmlExporter {
     fun export(track: Track): String {
-        var result = "<?xml version="1.0" encoding="UTF-8"?>
-"
-        result += "<kml xmlns="http://www.opengis.net/kml/2.2">
-"
-        result += "  <Document>
-"
-        result += "    <name>" + track.name + "</name>
-"
-        result += "    <Placemark>
-"
-        result += "      <name>" + track.name + "</name>
-"
-        result += "      <LineString>
-"
-        result += "        <coordinates>
-"
+        var result = """<?xml version="1.0" encoding="UTF-8"?>""" + "\n"
+        result += """<kml xmlns="http://www.opengis.net/kml/2.2">""" + "\n"
+        result += "  <Document>\n"
+        result += "    <name>" + track.name + "</name>\n"
+        result += "    <Placemark>\n"
+        result += "      <name>" + track.name + "</name>\n"
+        result += "      <LineString>\n"
+        result += "        <coordinates>\n"
         for (p in track.points) {
-            result += "          " + p.longitude + "," + p.latitude + "," + p.altitude + "
-"
+            result += "          " + p.longitude + "," + p.latitude + "," + p.altitude + "\n"
         }
-        result += "        </coordinates>
-"
-        result += "      </LineString>
-"
-        result += "    </Placemark>
-"
-        result += "  </Document>
-"
+        result += "        </coordinates>\n"
+        result += "      </LineString>\n"
+        result += "    </Placemark>\n"
+        result += "  </Document>\n"
         result += "</kml>"
         return result
     }
