e271ae7a88ff8348b48349b31244248211a7470b
armandocoroneo <armandocoroneo@gmail.com>
2026-07-30 10:23:44 -0400

    Refactor GpxExporter to use track point time directly

--- app/src/main/java/com/example/gpstracker/GpxExporter.kt
+++ app/src/main/java/com/example/gpstracker/GpxExporter.kt
@@ -1,37 +1,20 @@
 package com.example.gpstracker
 
-import java.text.SimpleDateFormat
-import java.util.*
-
 object GpxExporter {
     fun export(track: Track): String {
-        val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US)
-        sdf.timeZone = TimeZone.getTimeZone("UTC")
-        var result = "<?xml version="1.0" encoding="UTF-8"?>
-"
-        result += "<gpx version="1.1" creator="GPSTracker" xmlns="http://www.topografix.com/GPX/1/1">
-"
-        result += "  <trk>
-"
-        result += "    <name>" + track.name + "</name>
-"
-        result += "    <trkseg>
-"
+        var result = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+        result += "<gpx version=\"1.1\" creator=\"GPSTracker\">\n"
+        result += "  <trk>\n"
+        result += "    <name>" + track.name + "</name>\n"
+        result += "    <trkseg>\n"
         for (p in track.points) {
-            val time = sdf.format(Date(p.timestamp))
-            result += "      <trkpt lat="" + p.latitude + "" lon="" + p.longitude + "">
-"
-            result += "        <ele>" + p.altitude + "</ele>
-"
-            result += "        <time>" + time + "</time>
-"
-            result += "      </trkpt>
-"
+            result += "      <trkpt lat=\"" + p.latitude + "\" lon=\"" + p.longitude + "\">\n"
+            result += "        <ele>" + p.altitude + "</ele>\n"
+            result += "        <time>" + p.time + "</time>\n"
+            result += "      </trkpt>\n"
         }
-        result += "    </trkseg>
-"
-        result += "  </trk>
-"
+        result += "    </trkseg>\n"
+        result += "  </trk>\n"
         result += "</gpx>"
         return result
     }
