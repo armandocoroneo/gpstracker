748a5573a511c59cd24ec3f5d63b3b24bfcae599
armandocoroneo <armandocoroneo@gmail.com>
2026-07-30 10:43:17 -0400

    Remove time from CSV export format

--- app/src/main/java/com/example/gpstracker/CsvExporter.kt
+++ app/src/main/java/com/example/gpstracker/CsvExporter.kt
@@ -2,9 +2,9 @@ package com.example.gpstracker
 
 object CsvExporter {
     fun export(track: Track): String {
-        var result = "latitude,longitude,altitude,time\n"
+        var result = "latitude,longitude,altitude\n"
         for (p in track.points) {
-            result += p.latitude.toString() + "," + p.longitude.toString() + "," + p.altitude.toString() + "," + p.time.toString() + "\n"
+            result += p.latitude.toString() + "," + p.longitude.toString() + "," + p.altitude.toString() + "\n"
         }
         return result
     }
